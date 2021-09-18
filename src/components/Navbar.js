b2beb8533f3ff307644bc4d4d7aa51e121735833
Dupthala Arun Vamshi <arunvamshi1996@gmail.com>
2021-09-18 11:47:02 +0530

    Shopping-Cart

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+ const Navbar = ()=>{
+    return(
+            <nav className="nav-wrapper">
+                <div className="container">
+                    <Link to="/" className="brand-logo">Shopping</Link>
+                    
+                    <ul className="right">
+                        <li><Link to="/">Shop</Link></li>
+                        <li><Link to="/cart">My cart</Link></li>
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    </ul>
+                </div>
+            </nav>  
+    )
+}
+
+export default Navbar;
